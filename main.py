c93750fdfa6b32fe5b031afd836c2c8e38742bf2
i'm alex <kostencko.alexander2012@gmail.com>
2023-06-12 22:08:57 +0300

    structured code

--- main.py
+++ main.py
@@ -20,14 +20,6 @@ save = SaveDocument(document=document)
 # read_document.read_list_data(list_data=list_data, read_line=True)
 
 
-# добавляем текст в начало ячейки
-# change.add_data_start(cell_past='AE', text="В режиме ожидания: ")
-
-
-# удаяем весь текст с одной ячееки и добавляем в другую ячейку
-# change.move_to_other_cell(cell_move='AG', cell_past='AI')
-
-
 # Для проверки на заглавную букву в строке
 # search.checking_is_title()
 
@@ -36,21 +28,29 @@ save = SaveDocument(document=document)
 # search.serch_text(text=["NFC"])
 
 
-# обьяденяем данные столбцов в один столбец
-# change.join_columns_text(save_column='AH', join_columns=['AE', 'AF', 'AG'], join_separator=' x ', end_text='см')
+# добавляем текст в начало ячейки
+# change.add_data_start(cell_past='AE', text="В режиме ожидания: ")
 
 
-# удаляем текст что ищем с одной ячейки и добавляем в другую
-# change.serch_move_past(search="Функции: время, звонки, будильник, GPS трекер, Anti-Lost, шагомер, сигнал SOS, мониторинг передвижения, Geo – зоны", cell_move="AC", cell_past="AE")
+# добавляем текст во все ячейки в столбце
+# change.add_data_to_colums(cell_past='AE', text="test")
+
+
+# удаяем весь текст с одной ячееки и добавляем в другую ячейку
+# change.move_to_other_cell(cell_move='AG', cell_past='AI')
 
 
 # удаляем текст со всех ячейк в столбце
 # change.delete_data_to_column(cell_move='AI', text="При интенсивном использовании: До 24 ч")
 
 
-# добавляем текст во все ячейки в столбце
-change.add_data_to_colums(cell_past='AE', text="test")
+# удаляем текст что ищем с одной ячейки и добавляем в другую
+# change.serch_move_past(search="Функции: время, звонки, будильник, GPS трекер, Anti-Lost, шагомер, сигнал SOS, мониторинг передвижения, Geo – зоны", cell_move="AC", cell_past="AE")
+
+
+# обьяденяем данные столбцов в один столбец
+# change.join_columns_text(save_column='AH', join_columns=['AE', 'AF', 'AG'], join_separator=' x ', end_text='см')
 
 
 # указываем название нового файла для сохранения
-save.save(new_name="new")
+# save.save(new_name="new")
